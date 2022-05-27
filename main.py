f79e636faeb454b6297c7d674683136db27147bf
CLOUDUH <clouduh@outlook.com>
2022-05-27 22:56:08 +0800

    all done and ready to test

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import numpy as np
 from scipy.interpolate import griddata
 import pandas as pd
 import numpy.matlib
-
-
 ## Initialization
 
 # Load battert basic value
@@ -28,64 +26,11 @@ E_a = 18461 # Activation energy for cycle aging J/mol
 R = 8.314 # Ideal gas constant J/(kg*K)
 alpha = 32 # Coefficient for aging acceleration caused by the current
 
-# def PSO(N, d, ger):
-
-#     x = np.zeros((N,d))
-#     v = np.zeros((N,d))
-#     iter = 1
-#     w = 0.729
-#     c1 = 1.49115
-#     c2 = 1.49115
-#     beta = 1
-
-#     Ilimit = np.matlib.repmat(np.array([[0],[3.3]]),1,d)
-#     vlimit = np.matlib.repmat(np.array([[- 0.33],[0.33]]),1,d)
-
-#     lsat = np.zeros((N,d))
-#     hsat = np.zeros((N,d))
-
-
-
-# # f= @(SoH, t)(1 - beta) * (1 - SoH) + beta * t / t_m; # Objective function
-#     f1 = lambda t = None: beta * t / t_m
-
-#     for i in np.arange(1,d+1).reshape(-1):
-#         if i == 1:
-#             x[:,i] = np.matlib.repmat(Ilimit(1,i),N,1) + (Ilimit(2,i) - Ilimit(1,i)) * np.random.rand(N,1)
-#         else:
-#             x[:,i] = np.matlib.repmat(Ilimit(1,i),N,1) + (x(i - 1) - Ilimit(1,i)) * np.random.rand(N,1)
-#         v[:,i] = np.matlib.repmat(vlimit(1,i),N,1) + (vlimit(2,i) - vlimit(1,i)) * np.random.rand(N,1)
-
-#     xm = x
-#     ym = np.zeros((1,d))
-#     SoH = np.zeros((ger,N))
-#     t = np.zeros((ger,N))
-#     fx = np.zeros((ger,N))
-#     fxm = np.zeros((N,1))
-#     fym = - inf
-
-#     while iter <= ger:
-
-#         for j in np.arange(1,N+1).reshape(-1):
-#             SoH[iter,j],t[iter,j] = BatChrg(x(j,:))
-#             # fx(iter, j) = f(SoH(iter, j), t(iter,j)) # Optimal function value
-#             fx[iter,j] = f1(t(iter,j))
-#             if fxm(j) > fx(iter,j):
-#                 fxm[j] = fx(iter,j)
-#                 xm[j,:] = x(j,:)
-#         if fym > np.amin(fxm):
-#             fym,nmin = np.amin(fxm)
-#             ym = xm(nmin,:)
-#         v = v * w + c1 * rand * (xm - x) + c2 * rand * (np.matlib.repmat(ym,N,1) - x)
-#         for k in np.arange(1,d+1).reshape(-1):
-#             v[v[:,k] < vlimit[1,k]] = vlimit(1,k)
-#             v[v[:,k] > vlimit[2,k]] = vlimit(2,k)
-#         x = x + v
-#         for k in np.arange(1,d+1).reshape(-1):
-#             x[x[:,k] < Ilimit[1,k]] = Ilimit(1,k)
-#             x[x[:,k] > Ilimit[2,k]] = Ilimit(2,k)
-#         x
-#         iter = iter + 1
+w = 0.729 # Inertial weight
+c1 = 1.49115 # Self learning factor
+c2 = 1.49115 # Swarm learning factor 
+beta = 1 # Weight coefficient 1: fastest; 0: healthiest
+ger = 50 # The maximum number of iterations 
 
 def ECM(I = None, Temp = None, SoC = None): 
     """Battery 1-RC Equivalent Circuit Model
@@ -165,18 +110,17 @@ def nCC(I = None,SoC = None,SoCRange = None,Qloss = None,SoH = None,Temp = None,
 
     while SoC <= SoCRange:
 
-        if I == 0:
+        if I <= 0.01:
             break
 
         [_, SoC] = ECM(I, Temp, SoC)
         Temp = ThM(I, Temp, SoC)
         [Qloss, SoH] = AM(I, Temp, Qloss)
-        print(SoC, Temp, Qloss)
         i = i + 1
 
     return [SoC,Qloss,SoH,Temp,i]
 
-def BatChrg(CC): 
+def BatChrg(CC, iter, swarm): 
     """Battery Charging Function
     Input:
         CC: Battery charging constant-current(5-1 matrix)
@@ -190,7 +134,6 @@ def BatChrg(CC):
     SoH = 1 - ((Qloss / Qe) / 0.2)
     Temp = 298.15
     i = 1
-
     SoCRange = [0.2,0.4,0.6,0.8,1.0]
 
     [SoC,Qloss,SoH,Temp,i] = nCC(CC[1],SoC,SoCRange[1],Qloss,SoH,Temp,i)
@@ -199,9 +142,89 @@ def BatChrg(CC):
     [SoC,Qloss,SoH,Temp,i] = nCC(CC[4],SoC,SoCRange[4],Qloss,SoH,Temp,i)
     [SoC,Qloss,SoH,Temp,i] = nCC(CC[5],SoC,SoCRange[5],Qloss,SoH,Temp,i)
     t = (i - 1) * 0.05
-    return SoH,t
+    print("Battery Charge Process", iter, swarm)
+    return [SoH,t]
+
+def PSO(N, d, ger):
+    
+    iter = 1  # Initial iteration
+
+    N = 20
+    d = 5
+
+    x = np.zeros((N,d)) # Pariticle Position (N-d)
+    v = np.zeros((N,d)) # Pariticle Velcocity (N-d)
+
+    Ilimit = np.matlib.repmat(np.array([[0],[3.3]]),1,d) # Charging current limits (2-d)
+    vlimit = np.matlib.repmat(np.array([[-0.33],[0.33]]),1,d) # Velocity limits (2-d)
+
+    # lsat = np.zeros((N,d))
+    # hsat = np.zeros((N,d))
+
+    f1 = lambda t: beta * t / t_m # Object function
+
+    for i in range(d):
+        if i == 0:
+            x[:,i] = np.matlib.repmat(Ilimit[0,i],1,N) + (Ilimit[1,i] - Ilimit[0,i]) * np.random.rand(1,N)
+        else:
+            x[:,i] = np.matlib.repmat(Ilimit[0,i],1,N) + (x[:,i-1] - Ilimit[0,i]) * np.random.rand(1,N)
+        v[:,i] = np.matlib.repmat(vlimit[0,i],1,N) + (vlimit[1,i] - vlimit[0,i]) * np.random.rand(1,N)
+
+    xm = x # The best known position of particle (N-d)
+    ym = np.zeros((1,d)) # The best known position of entire swarm (1-d)
+    SoH = np.zeros((ger,N)) # SoH of each iteration of each particle (ger-N)
+    t = np.zeros((ger,N)) # Charging time of each iteration of each particle (ger-N)
+    fx = np.zeros((ger,N)) # Objective function value of each iteration of each particle (ger-N)
+    fxm = np.zeros((N,1)) # Optimal objective function value of particle (N-1)
+    fym = float("-inf") # Global optimal objective function value
+
+    while iter <= ger:
+
+        for j in range(N):
+
+            [SoH[iter,j],t[iter,j]]= BatChrg(x[j],iter,j) # Battery simulation
+            fx[iter,j] = f1(t[iter,j]) # Optimal function value
+
+            if fxm[j] > fx[iter,j]:
+                fxm[j] = fx[iter,j]
+                xm[j] = x[j]
+
+        if fym > np.amin(fxm):
+            fym = np.amin(fxm)
+            nmin = np.argmin(fxm)
+            ym = xm(nmin)
+
+        v = v * w + c1 * np.random.rand() * (xm - x) + c2 * np.random.rand() * (np.matlib.repmat(ym,N,1) - x)
+
+        # Velcocity Saturation
+        for k in range(d):
+            hsat = np.where(v[:,k] < vlimit[0,k], 1, 0)
+            lsat = np.where(v[:,k] > vlimit[1,k], 2, 0)
+
+            for j in range(N):
+                if hsat[j] == 1:
+                    v[j,k] = vlimit[0,k]
+                elif lsat[j] == 2:
+                    v[j,k] = vlimit[1,k]
+
+        x = x + v # Updating position
+
+        # Velcocity Saturation
+        for k in range(d): 
+            hsat = np.where(x[:,k] < Ilimit[0,k], 1, 0)
+            lsat = np.where(x[:,k] > Ilimit[1,k], 2, 0)
+
+            for j in range(N):
+                if hsat[j] == 1:
+                    x[j,k] = Ilimit[0,k]
+                elif lsat[j] == 2:
+                    x[j,k] = Ilimit[1,k]
+    
+        iter = iter + 1
+        print(iter)
+
+    return [SoH,t,ym,fym,fx,fxm]
 
 if __name__ == '__main__':
-    a = [1, 1, 0, 1, 2]
-    BatChrg(a)
+    PSO(20, 5, 50)
  
